1d28cb91f3fea9e9498b5c81bb9bac3361aff843
Tsames <tsames1@gmail.com>
2025-04-08 12:20:41 -0700

    Added new route that gets tasks by priority using a query paramaeter.

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,5 +1,6 @@
 package com.example.tsames
 
+import Priority
 import com.example.tsames.model.TaskRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -15,6 +16,30 @@ fun Application.configureRouting() {
                 contentType = ContentType.parse("text/html"),
                 text = tasks.tasksAsTable()
             )
+        }
+
+        get ("/tasks/byPriority/{priority}") {
+            val priorityAsText = call.parameters["priority"]
+            // If our query param does not exist, return 400
+            if (priorityAsText == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+            }
+
+            // Else Try processing the request
+            try {
+                val priority = Priority.valueOf(priorityAsText)
+                val tasks = TaskRepository.tasksByPriority(priority)
+
+                if (tasks.isEmpty()) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+
+                call.respondText(contentType = ContentType.parse("text/html"), text = tasks.tasksAsTable())
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
         }
     }
 }
