8714b77f64583fe71293bac2c610062d4a71db51
Tsames <tsames1@gmail.com>
2025-04-07 17:25:48 -0700

    Init Commit. Following Ktor guide.

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,30 @@
+package com.example.tsames
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<IllegalStateException> { call, cause ->
+            call.respondText("App in illegal state as ${cause.message}")
+        }
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        staticResources("/content", "mycontent")
+        get("/test1") {
+            val text = "<h1>Hello From Ktor</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
+        }
+        get("/error-test") {
+            throw IllegalStateException("Too Busy")
+        }
+    }
+}
