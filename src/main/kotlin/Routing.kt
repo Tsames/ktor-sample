139e6f4b0b6b760c56befe7a4758c5407850daae
Tsames <tsames1@gmail.com>
2025-04-08 12:01:59 -0700

    Refactor the task model by encapsulating it within a repository.

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,30 +1,20 @@
 package com.example.tsames
 
+import com.example.tsames.model.TaskRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.http.content.*
-import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import tasksAsTable
 
 fun Application.configureRouting() {
-    install(StatusPages) {
-        exception<IllegalStateException> { call, cause ->
-            call.respondText("App in illegal state as ${cause.message}")
-        }
-    }
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-        staticResources("/content", "mycontent")
-        get("/test1") {
-            val text = "<h1>Hello From Ktor</h1>"
-            val type = ContentType.parse("text/html")
-            call.respondText(text, type)
-        }
-        get("/error-test") {
-            throw IllegalStateException("Too Busy")
+        get("/tasks") {
+            val tasks = TaskRepository.allTasks()
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = tasks.tasksAsTable()
+            )
         }
     }
 }
