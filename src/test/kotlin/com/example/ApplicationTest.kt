98471025de6b5b9bb7ac428b4cd031bb5dcd479d
Tsames <tsames1@gmail.com>
2025-04-08 12:32:38 -0700

    Added unit tests for the new tasks by priority route.

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,35 +1,47 @@
+package com.example
+
 import com.example.tsames.module
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import org.junit.Assert.assertEquals
 import org.junit.Test
 import kotlin.test.assertContains
+import kotlin.test.assertEquals
 
 class ApplicationTest {
 
     @Test
-    fun testRoot() = testApplication {
+    fun tasksCanBeFoundByPriority() = testApplication {
         application {
             module()
         }
-        val response = client.get("/")
+
+        val response = client.get("/tasks/byPriority/Medium")
+        val body = response.bodyAsText()
 
         assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals("Hello World!", response.bodyAsText())
+        assertContains(body, "Mow the lawn")
+        assertContains(body, "Paint the fence")
     }
 
     @Test
-    fun testNewEndpoint() = testApplication {
+    fun invalidPriorityProduces400() = testApplication {
         application {
             module()
         }
 
-        val response = client.get("/test1")
+        val response = client.get("/tasks/byPriority/Invalid")
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+    }
 
-        assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals("html", response.contentType()?.contentSubtype)
-        assertContains(response.bodyAsText(), "Hello From Ktor")
+    @Test
+    fun unusedPriorityProduces404() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/tasks/byPriority/Vital")
+        assertEquals(HttpStatusCode.NotFound, response.status)
     }
 }
