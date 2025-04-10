adc1b1881545d4a84723503676deee1e0b35f147
Tsames <tsames1@gmail.com>
2025-04-10 13:41:20 -0700

    Added post request, html form to submit a post request, and associated test for the new post route.

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,15 +1,20 @@
 package com.example.tsames
 
 import Priority
+import Task
 import com.example.tsames.model.TaskRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.request.*
 import tasksAsTable
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/task-ui", "task-ui")
+
         get("/tasks") {
             val tasks = TaskRepository.allTasks()
             call.respondText(
@@ -18,7 +23,7 @@ fun Application.configureRouting() {
             )
         }
 
-        get ("/tasks/byPriority/{priority}") {
+        get("/tasks/byPriority/{priority}") {
             val priorityAsText = call.parameters["priority"]
             // If our query param does not exist, return 400
             if (priorityAsText == null) {
@@ -38,6 +43,38 @@ fun Application.configureRouting() {
 
                 call.respondText(contentType = ContentType.parse("text/html"), text = tasks.tasksAsTable())
             } catch (e: Exception) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
+        }
+
+        post("/tasks") {
+            val formContent = call.receiveParameters()
+
+            val params = Triple(
+                formContent["name"] ?: "",
+                formContent["description"] ?: "",
+                formContent["priority"] ?: "",
+            )
+
+            if (params.toList().any { it.isEmpty() }) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@post
+            }
+
+            try {
+                val priority = Priority.valueOf((params.third))
+                TaskRepository.addTask(
+                    Task(
+                        params.first,
+                        params.second,
+                        priority,
+                    )
+                )
+
+                call.respond(HttpStatusCode.NoContent)
+            } catch (ex: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest)
+            } catch (ex: IllegalStateException) {
                 call.respond(HttpStatusCode.BadRequest)
             }
         }
