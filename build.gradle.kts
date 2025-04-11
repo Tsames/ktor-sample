b044e60febcfe88bf5e14ba772a528f404d30440
Tsames <tsames1@gmail.com>
2025-04-10 20:09:16 -0700

    Adding tests back

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,4 +29,5 @@ dependencies {
     implementation(libs.ktor.server.config.yaml)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.ktor.client.content.negotiation)
 }
